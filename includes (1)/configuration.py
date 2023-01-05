f603706128032203e38393ddcc64d9860b8c744c
habeebaramide@yahoo.com <habeebaramide@yahoo.com>
2023-01-05 12:21:15 +0000

    Include commit

---
+++ includes (1)/configuration.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+raw_folder_path = "/mnt/formulalake/raw"
+processed_folder_path ="/mnt/formulalake/processed"
+presentation_folder_path = "/mnt/formulalake/presentation"
+
+# COMMAND ----------
+
+
